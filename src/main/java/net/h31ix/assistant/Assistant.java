8741f0e8c084335dc36e44ee15e1ee7788070665
Adam <admin@h31ix.net>
2011-11-06 22:51:43 -0500

    Updated src.

---
+++ src/main/java/net/h31ix/assistant/Assistant.java
@@ -0,0 +1,171 @@
+package net.h31ix.assistant;
+
+/*
+ * Seen for Bukkit
+ * 
+ * @author H31IX
+ * 
+ * H31IX.NET
+ */
+
+import java.io.File;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import java.text.SimpleDateFormat;
+import java.lang.System;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.Event.Type;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+
+public class Assistant extends JavaPlugin {
+    private Configuration config;
+    private AssistantPlayerListener playerListener = new AssistantPlayerListener(this);
+    public static Assistant plugin;
+    public final Logger logger = Logger.getLogger("Minecraft");
+    
+    
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+        System.out.println(this + " is now disabled!");
+    }
+    
+
+    public void onEnable() {
+    PluginManager pluginManager = getServer().getPluginManager();
+    pluginManager.registerEvent(org.bukkit.event.Event.Type.PLAYER_JOIN, playerListener, org.bukkit.event.Event.Priority.Monitor, this);
+    pluginManager.registerEvent(org.bukkit.event.Event.Type.PLAYER_QUIT, playerListener, org.bukkit.event.Event.Priority.Monitor, this);
+    new File("plugins/Seen").mkdir();
+		File configFile = new File("plugins/Seen/config.yml");
+		if(!configFile.exists()) {
+		    try {
+		    	configFile.createNewFile();
+		    } catch(Exception e) {
+		    }
+		}
+	    config = getConfiguration(); 
+		if (configFile.length()==0) {
+		    config.save(); 		
+		}
+	    config.load(); 
+            getCommand("seen").setExecutor(new CommandExecutor() {
+            public boolean onCommand(CommandSender cs, Command cmnd, String alias, String[] args) {
+                Player player = (Player)cs;
+                String safenick = player.getName().toLowerCase().replaceAll("'", "\"");
+                String safenick1 = safenick.replaceAll("§f", "");
+                if (args.length > 0) {
+                String cel = (args[0].toLowerCase());
+                String seen = config.getString(cel);
+                String first = config.getString(cel+"+");
+                if (seen != null)
+                {
+                Player target = Bukkit.getServer().getPlayer(args[0]);
+                long lStartTime = Long.parseLong(seen);
+                long lEndTime = new Date().getTime(); //end time
+                long difference = lEndTime - lStartTime; //check different
+                long finaltime = difference/(60*1000);
+                String operation = "minute(s)";
+                if (finaltime > 60)
+                {
+                    finaltime = finaltime/60;
+                    operation = "hour(s)";
+                if (finaltime > 24)
+                {
+                    finaltime = finaltime/24;
+                    operation = "day(s)";
+                }
+                }
+                
+                if (first != null)
+                {      
+                long lStartTime1 = Long.parseLong(first);
+                long lEndTime1 = new Date().getTime(); //end time
+                long difference1 = lEndTime1 - lStartTime1; //check different
+                long finaltime1 = difference1/(60*1000);
+                String operation2 = "minute(s)";
+                if (finaltime1 > 60)
+                {
+                    finaltime1 = finaltime1/60;
+                    operation2 = "hour(s)";
+                if (finaltime1 > 24)
+                {
+                    finaltime1 = finaltime1/24;
+                    operation2 = "day(s)";
+                }
+                }
+                
+                
+                if (cel.equalsIgnoreCase(safenick1)){
+                    player.sendMessage(ChatColor.RED + "I am superman.");
+                }
+                else {
+                List list = getServer().matchPlayer(cel);
+                if(list.size() == 1){
+                    player.sendMessage(ChatColor.GREEN + cel+" is online right now! Say hey!");
+                }
+                else {
+                if (seen==null) {
+                    player.sendMessage(ChatColor.RED + "That player has never been here before.");
+                }
+                else {    
+                player.sendMessage(ChatColor.RED + cel+" was last seen on this server "+finaltime+" "+operation+" ago.");
+                player.sendMessage(ChatColor.RED + cel+"'s first logon was "+finaltime1+" "+operation2+" ago.");
+                }
+                }
+                }
+                } else { player.sendMessage(ChatColor.RED + "That player has never been here before."); }
+                } else { player.sendMessage(ChatColor.RED + "That player has never been here before."); }
+                }else player.sendMessage(ChatColor.RED + "Usage: /seen <username>");
+                return true;
+            }
+           });
+            getCommand("playtime").setExecutor(new CommandExecutor() {
+            public boolean onCommand(CommandSender cs, Command cmnd, String alias, String[] args) {
+                Player player = (Player)cs;
+                String safenick = player.getName().toLowerCase().replaceAll("'", "\"");
+                String safenick1 = safenick.replaceAll("§f", "");
+                if (args.length < 1) {
+                String first = config.getString(safenick1+"+");
+                  if (first != null)
+                {      
+                long lStartTime1 = Long.parseLong(first);
+                long lEndTime1 = new Date().getTime(); //end time
+                long difference1 = lEndTime1 - lStartTime1; //check different
+                long finaltime1 = difference1/(60*1000);
+                String operation2 = "minute(s)";
+                if (finaltime1 > 60)
+                {
+                    finaltime1 = finaltime1/60;
+                    operation2 = "hour(s)";
+                if (finaltime1 > 24)
+                {
+                    finaltime1 = finaltime1/24;
+                    operation2 = "day(s)";
+                }
+                }
+                player.sendMessage(ChatColor.RED + "Your first login was "+finaltime1+" "+operation2+" ago.");
+                }
+            }else player.sendMessage(ChatColor.RED + "Usage: /playtime");
+                return true;
+            }
+           });
+        System.out.println(this + " is now enabled with ERKUL!"); 
+    } 
+}    
+
---
+++ src/main/java/net/h31ix/assistant/AssistantPlayerListener.java
@@ -0,0 +1,76 @@
+package net.h31ix.assistant;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Logger;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
+
+public class AssistantPlayerListener extends PlayerListener {
+    private Configuration config;
+    private final Assistant plugin;
+    public final Logger logger = Logger.getLogger("Minecraft");
+    
+       
+    
+    public AssistantPlayerListener(Assistant plugin) {
+        this.plugin = plugin;
+    }
+    
+            
+    @Override
+    public void onPlayerQuit(PlayerQuitEvent event) {
+                Player player = event.getPlayer();
+                String safenick = player.getName().toLowerCase().replaceAll("'", "\"");
+                String safenick1 = safenick.replaceAll("§f", "");
+                config = plugin.getConfiguration();
+                config.load();
+                long lStartTime = new Date().getTime(); //start time
+                config.setProperty(safenick1, lStartTime);
+                config.save();
+    }
+    
+    @Override
+    public void onPlayerJoin (PlayerJoinEvent event)
+    {
+        Player player = event.getPlayer();
+        String safenick = player.getName().toLowerCase().replaceAll("'", "\"");
+        String safenick1 = safenick.replaceAll("§f", "");
+        String safenick2 = (safenick1+"+");
+        config = plugin.getConfiguration();
+        String seen = config.getString(safenick1); 
+        String seen2 = config.getString(safenick2);
+        if(seen == null || seen2 == null)
+        {
+            long start1 = new Date().getTime(); //start time
+            config.load();
+            config.setProperty(safenick2, start1);
+            config.save();
+        }
+            
+    /*public void onPlayerJoin (PlayerJoinEvent event) {
+        System.out.println("An onplayerlogin has occured");
+                Player player = event.getPlayer();
+                String safenick = player.getName().toLowerCase().replaceAll("'", "\"");
+                String safenick1 = safenick.replaceAll("§f", "");
+                String safenick2 = (safenick1+"+");
+                String seen = config.getString(safenick1);
+                if (seen == null)
+                {
+                config = plugin.getConfiguration();
+                config.load();
+                long lStartTime = new Date().getTime(); //start time
+                config.setProperty("derpy", lStartTime);
+                config.save();
+                }*/
+    }
+}
